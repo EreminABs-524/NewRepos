7109b85a37c80d207c9bc6c6815d72377bcd34e8
EreminABs-524 <dima.dmitr.eromin@gmail.com>
2026-03-26 17:36:40 +0700

    Отправка функции

---
+++ circleFunction.cpp
@@ -0,0 +1,10 @@
+//Комментарий для функции
+float circleLength(int radius){
+	return radius*2*3.14;
+}
+float circleArea(int radius){
+	return 3.14*radius*radius;
+}
+float circleSector(int radius, int degree){
+	return (radius*radius*degree*3.14)/360;
+}
